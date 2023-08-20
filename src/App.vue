c0f8426b20b4307c4e3abe728c9bedc8d46e5416
jorge perdomo <jorgamow@gmail.com>
2023-08-19 22:20:03 -0500

    Se traen todas las cryptos pero aún no se logra la reactividad al buscar

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,53 @@
 <template>
   
-  <h1>Hello World</h1>
+  <h1>Crypto Precios</h1>
+
+  <input @keyup="buscarMoneda()" :value="valorBusqueda" />
 
   <table>
     <thead>
       <tr>
-        <th>Header 1</th>
-        <th>Header 2</th>
-        <th>Header 3</th>
+        <th>Moneda</th>
+        <th>Precio (eur)</th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="coin in coins" :key="coin.id">
-             
+      <tr v-for="coin in monedasFiltradas" :key="coin.id">
+        <td><img :src="coin.image"> {{ coin.name }} {{ coin.symbol }}</td>       
+        <td>{{ coin.current_price }}</td>       
       </tr>
     </tbody>
   </table>
 
 </template>
 
-<script>
-export default {
+<script setup>
 
-  name: 'App',
+  import { onMounted, ref } from 'vue'
+  let coins  = []
+  let monedasFiltradas = ref([])
+  let valorBusqueda;
+  let buscarMoneda = () => {
+    monedasFiltradas = coins.filter( elemento => elemento.name === valorBusqueda || elemento.symbol === valorBusqueda ) 
+  };
 
-  data(){
-    return {
-      coins: []
-    };
-  },
+  onMounted(() => {
+    fetch(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=es'
+    ).then( 
+      res => res.json() 
+    ).then( 
+    datos => { 
+      /* coins = datos; */ 
+      monedasFiltradas.value = datos; 
+      coins.value = datos;
+      console.log('coins: ', coins ); 
+    }).catch( error => console.error('Error en fetch: '+error.message) );
+  })
 
-  async mounted(){
-    const res = await fetch(
-      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en' 
-    );
-    const data = await res.json()
-    this.coins = data
-  }
-  
-}
 </script>
 
 
-<style></style>
+<style>
+
+</style>
